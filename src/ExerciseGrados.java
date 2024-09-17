b8e2697ed559522bc21c7aa8c049c0b4c2ba5f4c
Cristian-PinedaQ <crispdevps@gmail.com>
2024-09-16 20:43:13 -0500

    agregando funcionalidad de impresion a ExerciseGrados

--- src/ExerciseGrados.java
+++ src/ExerciseGrados.java
@@ -1,11 +1,13 @@
 public class ExerciseGrados {
     public static void main(String[] args) {
 
-        double celsius = 12;
-        double conversorFahrenheit = (celsius *1.8) + 32;
-        int temperaturaEntera = (int) conversorFahrenheit;
+        float celsius = 12;
+        float fahrenheit = (float) ((celsius *1.8) + 32);
+        int temperaturaEntera = (int) fahrenheit;
 
-        System.out.println(conversorFahrenheit + " °F");
+        String mensaje = String.format("%.1f grados Celsius, equivalen a %.1f grados Fahrenheit.", celsius, fahrenheit);
+        System.out.println(mensaje);
+        System.out.println(fahrenheit + " °F");
         System.out.println(temperaturaEntera + " °F");
     }
 }
